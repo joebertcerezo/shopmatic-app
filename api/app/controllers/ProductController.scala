063a94a0210d36e4ca785a08b5fe9add8f949889
Joebert L. Cerezo <jlcerezo@vauldex.com>
2024-11-29 15:18:53 +0800

    fix product controller

--- api/app/controllers/ProductController.scala
+++ api/app/controllers/ProductController.scala
@@ -8,6 +8,10 @@ import play.api.libs.json._
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import java.util.UUID
+
+import forms.ProductForm._
+
 import domain.product._
 
 @Singleton
@@ -18,12 +22,12 @@ class ProductController @Inject()
 ) extends BaseController {
   
 
-  def add(id: String) = Action.async(parse.json) { implicit request =>
-    request.body.validate[ProductCreate].fold(
-      error => Future.successful(BadRequest(JsError.toJson(error))),
-      data => productService.add(data, java.util.UUID.fromString(id)).value.map {
+  def add(id: UUID) = Action.async(parse.json) { implicit request =>
+    productForm.bindFromRequest().fold(
+      error => Future.successful(BadRequest(error.errors.map(_.message).mkString(", "))),
+      data => productService.add(data, id).value.map {
         case Left(msg) => BadRequest(msg)
-        case Right(msg) => Ok(Json.toJson(msg))
+        case Right(msg) => Ok(Json.obj("message" -> msg))
       }.recover{ _ => InternalServerError("Server Error.") }
     )
   }
